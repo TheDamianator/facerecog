2bfaa66e26aacc1386155db46101a0f5c36d39cd
TheDamianator <91943089+TheDamianator@users.noreply.github.com>
2024-02-26 10:53:51 +0100

    adjusted main

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import WebcamComponent from './WebcamComponent';
 import ResultsComponent from './ResultsComponent';
 import LoadModelsComponent from './LoadModelsComponent';
+
 const App = () => {
   const [results, setResults] = useState([]);
 
   return (
     <div>
       <h1>Live Facial Recognition</h1>
+      <LoadModelsComponent/>
       <WebcamComponent setResults={setResults} />
       <ResultsComponent results={results} />
-      <LoadModelsComponent/>
     </div>
   );
 };
