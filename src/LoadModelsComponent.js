e797c34f781e7b42b90f09793d752d2199d8d91f
Navisa Sabeti <navisasabeti10@gmail.com>
2024-02-26 10:07:57 +0100

    changed the models

--- src/LoadModelsComponent.js
+++ src/LoadModelsComponent.js
@@ -1,31 +1,26 @@
-import React, { useEffect } from 'react';
-import * as faceapi from 'face-api.js';
+import React, { useEffect } from "react";
+import * as faceapi from "face-api.js";
 
 const LoadModelsComponent = () => {
   useEffect(() => {
     const loadModels = async () => {
       try {
-        
         await Promise.all([
           faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
           faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
           faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         ]);
-        
-        console.log('Models loaded successfully');
+
+        console.log("Models loaded successfully");
       } catch (error) {
-        console.error('Error loading models:', error);
+        console.error("Error loading models:", error);
       }
     };
 
     loadModels();
   }, []);
 
-  return (
-    <div>
-      Loading face detection models...
-    </div>
-  );
+  return <div>Loading face detection models...</div>;
 };
 
 export default LoadModelsComponent;
